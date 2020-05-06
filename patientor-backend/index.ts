adb532dbb6bf86ca0c903aba8775b8f86d030f66
yc <10258156+yaochie@users.noreply.github.com>
2020-05-05 23:51:28 -0400

    exercise 9.9

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -3,9 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_request, response) => {
+app.get('/api/ping', (_request, response) => {
+    console.log('got pinged')
     response.send("pong");
 });
 
